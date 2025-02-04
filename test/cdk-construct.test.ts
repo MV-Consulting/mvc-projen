8f92005178a5d352a775b3a94f156540c1cf6197
Manuel Vogel <8409778+mavogel@users.noreply.github.com>
2025-02-04 10:46:29 +0100

    refactor(test): unit tests with cdkVersion check
    Signed-off-by: Manuel Vogel <8409778+mavogel@users.noreply.github.com>

--- test/cdk-construct.test.ts
+++ test/cdk-construct.test.ts
@@ -1,17 +1,27 @@
 import { synthSnapshot } from 'projen/lib/util/synth';
-import { MvcCdkConstructLibrary } from '../src';
+import { MvcCdkConstructLibrary, MvcCdkConstructLibraryOptions } from '../src';
+
+const minimalMvcCdkConstructLibraryOptions: MvcCdkConstructLibraryOptions = {
+  author: 'test',
+  authorAddress: 'test@test.com',
+  cdkVersion: '2.1.0',
+  name: 'test-project',
+  defaultReleaseBranch: 'main',
+  repositoryUrl: 'https://github.com/MV-Consulting/test-project.git',
+};
+
+describe('cdkVersion is >= 2.0.0', () => {
+  test('check', () => {
+    const project = new MvcCdkConstructLibrary(minimalMvcCdkConstructLibraryOptions);
+    synthSnapshot(project);
+  });
+});
+
 
 describe('configurations', () => {
   test('default all files written', () => {
-    const project = new MvcCdkConstructLibrary({
-      author: 'test',
-      authorAddress: 'test@test.com',
-      cdkVersion: '1.0.0',
-      name: 'test-project',
-      defaultReleaseBranch: 'main',
-      repositoryUrl: 'https://github.com/MV-Consulting/test-project.git',
-    });
-    const snap = synthSnapshot(project); //, { parseJson: false });
+    const project = new MvcCdkConstructLibrary(minimalMvcCdkConstructLibraryOptions);
+    const snap = synthSnapshot(project);
     expect(
       snap['.github/FUNDING.yaml'].indexOf('github: mavogel'),
     ).not.toEqual(-1);
